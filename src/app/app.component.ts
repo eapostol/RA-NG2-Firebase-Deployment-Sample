6cb75645d00bd4eb74715c3f58ae24cff805588a
Edward Apostol <edward.apostol.github@gmail.com>
2017-02-28 14:43:58 -0500

    #011: main app component updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
+
+// reference window object (contains document)
+import { WindowObjectService } from './dom/window-object.service';
+
+
+// test jquery;
+import * as $ from 'jquery';
+import {document} from "@angular/platform-browser/src/facade/browser";
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`
+  templateUrl: 'app.component.html',
+  providers:[WindowObjectService]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   name = 'Angular';
+
+  hasJQuery:Boolean = false;
+  windowRef:any;
+
+  constructor(private mainWindow:WindowObjectService){
+    this.windowRef = mainWindow.nativeWindow;
+    console.log(this.windowRef);
+    this.init();
+  }
+  private init(){
+    this.hasJQuery = this.checkJQuery();
+    if(this.hasJQuery){
+        this.onDocumentReady();
+    }
+  }
+
+  private onDocumentReady(){
+    $(document).ready(function(){
+      console.log(`document loaded - ${document}`);
+
+    })
+
+  }
+
+  ngAfterViewInit(){
+
+  }
+
+  private checkJQuery(){
+    return ($.isFunction($));
+  }
 }
